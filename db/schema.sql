c56064abb630990879a63ab4ecda528632993be2
JAPinell <japinell@yahoo.com>
2021-05-30 21:01:25 -0500

    Fixed schema and seed scripts

--- db/schema.sql
+++ db/schema.sql
@@ -3,31 +3,30 @@ CREATE DATABASE cms_db;
 
 USE cms_db;
 
-CREATE TABLE department(
+CREATE TABLE departments(
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role(
+CREATE TABLE roles(
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL (10,2),
   dept_id INT,
   INDEX dept_idx (dept_id),
-  FOREIGN KEY (dept_id) REFERENCES department(id),
+  FOREIGN KEY (dept_id) REFERENCES departments(id),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE employee(
+CREATE TABLE employees(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT DEFAULT NULL,
   INDEX role_idx (role_id),
-  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (role_id) REFERENCES roles(id),
   INDEX manager_idx (manager_id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id),
   PRIMARY KEY (id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -25,17 +25,17 @@ INSERT INTO roles (title, salary, dept_id)
 VALUES ("Lawyer", 90000, 4);
 
 -- Employees
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Mike", "Chan", 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Ashley", "Rodriguez", 3, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Kevin", "Tupik", 4, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Malia", "Brown", 5, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Sarah", "Lourd", 6, 5);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Tom", "Allen", 7, 6);
